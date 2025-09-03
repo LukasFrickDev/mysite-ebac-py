769f2c7562f2faec8b7a4f7f79da84d38a4f827a
Lukas Frick <contatolukasfrick@gmail.com>
2025-09-03 16:45:53 -0300

    exclusão de arquivos exercício

--- mysite/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path 
-from blog import views
-
-urlpatterns = [
-    path('', views.PostView.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
-]
